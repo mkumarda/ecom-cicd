955fb28edb9e4a7cb502f9cf744bdf44d8f3f4fb
mkumarda <mkumarda@gmail.com>
2024-09-23 23:46:22 +0530

    Initial Commit

---
+++ src/test/java/com/apple/ecom/EcomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apple.ecom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
